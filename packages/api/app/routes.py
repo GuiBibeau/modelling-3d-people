860a0eab64fffa292b53344ebb40e0bbde56952a
Guillaume Bibeau-laviolette <guillaumebibeau@filevine.com>
2021-10-12 09:08:24 -0400

    WIP pose estimation with cuda net working gradually

--- packages/api/app/routes.py
+++ packages/api/app/routes.py
@@ -1,6 +1,16 @@
+import torch
+import os
 from app import app
+from app import get_rect
+from app.external.pose_estimation.modules.load_state import load_state
+from app.external.pose_estimation.models.with_mobilenet import PoseEstimationWithMobileNet
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    print(os.getcwd())
+    net = PoseEstimationWithMobileNet()
+    checkpoint = torch.load("./app/external/pose_estimation/checkpoint_iter_370000.pth", map_location='cpu')
+    load_state(net, checkpoint)
+    get_rect(net.cuda(), ['/app/sample_images/test.png'], 512)
+    return "Hello World!"
